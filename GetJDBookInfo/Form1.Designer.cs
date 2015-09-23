b8ac34a5ef9b680a400d77715025134fe0529d4d
amwtke <amwtke@gmail.com>
2015-09-23 14:50:06 +0800

    first

---
+++ GetJDBookInfo/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace GetJDBookInfo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.bt_load = new System.Windows.Forms.Button();
+            this.tx_url = new System.Windows.Forms.TextBox();
+            this.rtx_info = new System.Windows.Forms.RichTextBox();
+            this.picBox = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.picBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // bt_load
+            // 
+            this.bt_load.Location = new System.Drawing.Point(2, 50);
+            this.bt_load.Name = "bt_load";
+            this.bt_load.Size = new System.Drawing.Size(75, 23);
+            this.bt_load.TabIndex = 0;
+            this.bt_load.Text = "load";
+            this.bt_load.UseVisualStyleBackColor = true;
+            this.bt_load.Click += new System.EventHandler(this.bt_load_Click);
+            // 
+            // tx_url
+            // 
+            this.tx_url.Location = new System.Drawing.Point(2, 22);
+            this.tx_url.Name = "tx_url";
+            this.tx_url.Size = new System.Drawing.Size(530, 22);
+            this.tx_url.TabIndex = 1;
+            this.tx_url.Text = "http://item.jd.com/11750397.html";
+            // 
+            // rtx_info
+            // 
+            this.rtx_info.Location = new System.Drawing.Point(2, 90);
+            this.rtx_info.Name = "rtx_info";
+            this.rtx_info.Size = new System.Drawing.Size(269, 356);
+            this.rtx_info.TabIndex = 2;
+            this.rtx_info.Text = "";
+            // 
+            // picBox
+            // 
+            this.picBox.Location = new System.Drawing.Point(277, 90);
+            this.picBox.Name = "picBox";
+            this.picBox.Size = new System.Drawing.Size(449, 521);
+            this.picBox.TabIndex = 3;
+            this.picBox.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(856, 623);
+            this.Controls.Add(this.picBox);
+            this.Controls.Add(this.rtx_info);
+            this.Controls.Add(this.tx_url);
+            this.Controls.Add(this.bt_load);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.picBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button bt_load;
+        private System.Windows.Forms.TextBox tx_url;
+        private System.Windows.Forms.RichTextBox rtx_info;
+        private System.Windows.Forms.PictureBox picBox;
+    }
+}
+
---
+++ GetJDBookInfo/Form1.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using Microsoft.Win32;
+using System.Runtime.Remoting.Messaging;
+using System.Runtime.InteropServices;
+using System.Net;
+namespace GetJDBookInfo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void bt_load_Click(object sender, EventArgs e)
+        {
+            if (IsInterNetConnected())
+            {
+                try
+                {
+                    string realUlr = tx_url.Text;
+                    HtmlHelper.HtmlFromWebReq req = new HtmlHelper.HtmlFromWebReq(
+                        realUlr, delegate(byte[] data)
+                        {
+                            //string constructedString = System.Text.Encoding.UTF8.GetString(data);
+                            string constructedString = System.Text.Encoding.GetEncoding("GB2312").GetString(data);
+                            HtmlAgilityPack.HtmlDocument doc = new HtmlAgilityPack.HtmlDocument();
+                            doc.LoadHtml(constructedString);
+                            HtmlAgilityPack.HtmlNode nodeTitle = doc.DocumentNode.SelectSingleNode("/html[1]/head[1]/title");
+                            string bookAuthor="";
+                            string abstraction = nodeTitle.InnerHtml;
+                            int posOfBookMark = abstraction.IndexOf("》");
+
+                            //获取最后一个）符号的位置
+                            int posOflastBrace = abstraction.IndexOf(")", posOfBookMark);
+                            int temp = posOflastBrace;
+                            while (temp != -1)
+                            {
+                                posOflastBrace = temp;
+                                temp = abstraction.IndexOf(")", temp+1);
+                            }
+
+                            if (posOflastBrace != -1)
+                                bookAuthor = abstraction.Substring(posOfBookMark + 2, posOflastBrace - posOfBookMark - 2);
+
+                            HtmlAgilityPack.HtmlNodeCollection _nodes = doc.DocumentNode.SelectNodes(@"//html[1]/head[1]/meta[@name='keywords']");
+                            foreach (HtmlAgilityPack.HtmlNode no in _nodes)
+                            {
+                                //no.GetAttributeValue("content","");
+                                string s = no.GetAttributeValue("content", "");
+                                string[] bookinfo = s.Split(new char[] { ',' });
+                                string bookname = bookinfo[0];
+                                if (!String.IsNullOrEmpty(bookinfo[1]))
+                                    bookAuthor = bookinfo[1];
+                                string bookPublisher = bookinfo[2];
+                                string bookISBN = bookinfo[3];
+                                SetText( bookname + "\n" + bookAuthor + "\n" + bookPublisher + "\n" + bookISBN);
+                            }
+
+                            //获取书籍图片
+                            HtmlAgilityPack.HtmlNode pBoxNode = doc.DocumentNode.SelectSingleNode(@"//html[1]/body[1]/div[@id='p-box']/div[3]/div[@id='product-intro']/div[@id='preview']/div[@id='spec-n1']");
+                            if (pBoxNode != null)
+                            {
+                                HtmlAgilityPack.HtmlNode image = pBoxNode.SelectSingleNode(@"//img[1]");
+                                if (image != null)
+                                {
+                                    string imagesrc = image.GetAttributeValue("src", "");
+                                    using(WebClient wc = new WebClient())
+                                    {
+                                        picBox.Load("http:"+imagesrc);
+                                    }
+                                    
+                                }
+                            }
+
+                        });
+
+                    req.BeginCreateHtml();
+                }
+                catch (Exception ex)
+                {
+                    throw ex;
+                }
+            }
+            else
+                MessageBox.Show("没有互联网！");
+        }
+        
+
+        [DllImport("winInet.dll")]
+        private static extern bool InternetGetConnectedState(
+        ref   int dwFlag,
+        int dwReserved
+        );
+        public static bool IsInterNetConnected()
+        {
+            System.Int32 dwFlag = new int();
+            return InternetGetConnectedState(ref   dwFlag, 0);
+            //MessageBox.Show("未连网!");   
+            //else   
+            //if((dwFlag   &   INTERNET_CONNECTION_MODEM)!=0)   
+            //MessageBox.Show("采用调治解调器上网。");   
+            //else   
+            //if((dwFlag   &   INTERNET_CONNECTION_LAN)!=0)   
+            //MessageBox.Show("采用网卡上网。");   
+        }
+
+        private delegate void SetRichTextBox(String s);
+        public void SetText(string txt)
+        {
+            if (this.rtx_info.InvokeRequired)
+            {
+                SetRichTextBox s = new SetRichTextBox(SetRt);
+                this.Invoke(s, new object[] { txt });
+            }
+            else
+            {
+                rtx_info.Text = txt;
+            }
+        }
+
+        private void SetRt(string text)
+        {
+            rtx_info.Text = text;
+        }
+
+    }
+
+}
